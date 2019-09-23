b549ad441f854b47fb6be5ff3c5cd7cfabf48f7a
user1 <user1@user1-PC>
2019-09-23 14:29:01 +0900

    detailSt.dr 추가

--- doDream/src/main/java/com/dodream/spring/project/controller/ProjectController2.java
+++ doDream/src/main/java/com/dodream/spring/project/controller/ProjectController2.java
@@ -1,10 +1,27 @@
 package com.dodream.spring.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dodream.spring.project.model.service.ProjectService;
+import com.dodream.spring.project.model.vo.Project;
 
 @Controller
 public class ProjectController2 {
 /// 주연씨
 	
+	@Autowired
+	private ProjectService pService;
+	
+	@RequestMapping("detailSt.dr")
+	public String prjDetailView(Integer pNo, Model model) {
+		int pno = pNo;
+		Project prj = pService.selectProject(pno);
+		
+		model.addAttribute("project", prj);
+		return "project/projectDetailStoty";
+	}
 
 }
