3c386d84ea3a5421988b43d12c854cf6c9d472b8
user1 <user1@user1-PC>
2019-09-25 18:18:35 +0900

    고객센터_공지사항, faq 수정_2019.09.25

--- doDream/src/main/java/com/dodream/spring/customerCenter/model/dao/faqDao.java
+++ doDream/src/main/java/com/dodream/spring/customerCenter/model/dao/faqDao.java
@@ -1,8 +1,13 @@
 package com.dodream.spring.customerCenter.model.dao;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.dodream.spring.customerCenter.model.vo.Faq;
+import com.dodream.spring.customerCenter.model.vo.Search;
  
 
 @Repository("faqDao")
@@ -10,5 +15,15 @@ public class faqDao {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public ArrayList<Faq> selectList() {
+		
+		return (ArrayList)sqlSession.selectList("centerMapper.faqSelect");
+	}
+	// FAQ 검색을 위한 dao
+	public ArrayList<Faq> searchList(Search search) {
+		
+		return (ArrayList)sqlSession.selectList("centerMapper.searchList",search);
+	}
 }
  
